6029c5733097030833356422a7c85cbdf1c33881
uppaltushar <40592029+uppaltushar@users.noreply.github.com>
2018-08-20 17:26:10 +0530

    Add files via upload

---
+++ assignment-2.py
@@ -0,0 +1,39 @@
+"""Question 1"""
+print("tushar uppal")
+
+
+"""Question 2"""
+s1="uppal"
+s2="tushar"
+print(s1+" "+s2)
+
+
+"""Question 3"""
+v1=input("Enter  ")
+v2=input("Enter  ")
+v3=int(input("age "))
+print("Name",v1+" "+v2)
+print("Age{}".format(v3))
+
+
+"""Question 4"""
+print('''"Let's Get Started"''')
+
+
+"""Question 5"""
+s="Acadview" 
+course="Python" 
+fees=5000 
+
+print("""S:-%s
+Fees:- %d
+Course : %s
+"""%(s,fees,course))
+
+
+
+"""Question 6"""
+pi=3.14
+r=float(input("Enter Radius of circle"))
+area=pi*(r**2)
+print("Area of circle:- {}".format(area))
